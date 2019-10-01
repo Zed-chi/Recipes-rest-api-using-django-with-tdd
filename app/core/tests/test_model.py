63a4fdbb0a0a090c05aad266bc74dea56c4e4d60
zed <zed@mail.ru>
2019-10-01 23:11:31 +0300

    added empty email test

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -8,8 +8,8 @@ class ModelTests(TestCase):
         email = "test@test.com"
         password = "secret"
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
