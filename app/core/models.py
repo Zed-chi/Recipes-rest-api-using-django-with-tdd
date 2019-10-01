740fce13e001af888c5bedd1b7af5591b1091988
zed <zed@mail.ru>
2019-10-01 23:11:39 +0300

    upd

--- app/core/models.py
+++ app/core/models.py
@@ -1,22 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    PermissionsMixin,
+    AbstractBaseUser,
+)
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra):
         """ creates and saves a new user """
-        user = self.model(
-            email=self.normalize_email(email),
-            **extra
-        )
+        if not email:
+            raise ValueError("User must have an email")
+        user = self.model(email=self.normalize_email(email), **extra)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        """ creates and saves a superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
 class UserModel(AbstractBaseUser, PermissionsMixin):
     """ Custom User Model with email insteead name"""
+
     email = models.EmailField(max_length=2555, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
