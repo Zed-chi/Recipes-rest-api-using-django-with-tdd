3c2c717b5362e0283939884a99a1a15a3231801b
alex <zed-chi@mail.ru>
2019-10-01 15:55:06 +0300

    Deleted test example files

--- app/app/test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-
-class CalcTests(TestCase):
-    def test_add_numbers(self):
-        """test adding numbers"""
-        self.assertEqual(add(3,8), 11)
-        
