aa60bc6a01663159cbff128e86339d65b8b77a8f
zed <zed@mail.ru>
2019-10-01 22:46:33 +0300

    added email case test

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -13,3 +13,14 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Tests the email is normalized """
+        email = "test@TEST.com"
+        password = "secret"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email.lower())
