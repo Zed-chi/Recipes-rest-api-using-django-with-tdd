1716c684a1a5404d4d60abd9405c17eb1c52f7b3
zed <zed@mail.ru>
2019-10-01 22:27:08 +0300

    added Usermodel

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra):
+        """ creates and saves a new user """
+        user = self.model(email=email, **extra)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    """ Custom User Model with email insteead name"""
+    email = models.EmailField(max_length=2555, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
