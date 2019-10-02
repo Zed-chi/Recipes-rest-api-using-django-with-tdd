56b520053b32b14f0577e1356a09ea069e593526
alex <zed-chi@mail.ru>
2019-10-02 13:39:17 +0300

    upd

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,7 +10,10 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Personal Info"), {"fields": ("name",)}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser")}
+        ),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
 
