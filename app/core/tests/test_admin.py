52eef58ad8705f2c092d974f4102b8a84a6fbe2e
alex <zed-chi@mail.ru>
2019-10-02 16:15:02 +0300

    added test for user add

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,3 +29,8 @@ class AdminSiteTest(TestCase):
         url = reverse("admin:core_usermodel_change", args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_usermodel_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
