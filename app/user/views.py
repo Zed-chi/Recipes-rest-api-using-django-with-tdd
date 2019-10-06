eb0df049b99b2fcbb990a5f3f271dd293eece9a8
zed <zed@mail.ru>
2019-10-06 19:32:57 +0300

    added view

--- app/user/views.py
+++ app/user/views.py
@@ -1,17 +1,30 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-    """ Create new user in system """
+    """Create new user in the system"""
 
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    """ Creates new auth token for user"""
+class AuthTokenView(ObtainAuthToken):
+    """Create a new auth token for the user"""
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage use profile update name and password"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrive and return authentication user"""
+        return self.request.user
