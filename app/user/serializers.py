83f762fed7fe8458ab25c5ca4dadc2c2a427ad82
zed <zed@mail.ru>
2019-10-06 16:29:22 +0300

    added auth serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,13 +9,31 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("email", "password", "name")
-        extra_kwargs = {
-            "password": {
-                "write_only": True,
-                "min_length": 5
-            }
-        }
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
         """ create new user with enc pass and return it """
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """ Serializer for auth object"""
+
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _("Unable to auth with provided creds")
+            raise serializers.ValidationError(msg, code="authentication")
+        attrs["user"] = user
+        return attrs
