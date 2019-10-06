9da3c87dbf34aa749fb604db329b84251fea48af
zed <zed@mail.ru>
2019-10-06 16:29:55 +0300

    added view fot auth token

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,17 @@
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """ Create new user in system """
+
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """ Creates new auth token for user"""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
