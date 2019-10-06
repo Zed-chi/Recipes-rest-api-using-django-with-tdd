b5b1e4268398dc520e54340d4af077d179481d54
zed <zed@mail.ru>
2019-10-07 00:32:26 +0300

    edit view tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
     """ Manage Tags in th database """
 
     authentication_classes = (TokenAuthentication,)
@@ -16,3 +18,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """ Return objects for current authed users """
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """ Creates neew tag """
+        serializer.save(user=self.request.user)
