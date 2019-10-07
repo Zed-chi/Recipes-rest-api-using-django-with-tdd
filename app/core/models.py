e4a4d5be2c4317e905a65dc2ca12a0c1bae5cd0b
alex <zed-chi@mail.ru>
2019-10-07 13:10:59 +0300

    added ing model

--- app/core/models.py
+++ app/core/models.py
@@ -42,8 +42,17 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 
     def __str__(self):
