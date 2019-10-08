5f6f533b98286f654a9ebfed833d625176e87888
zed <zed@mail.ru>
2019-10-08 21:19:14 +0300

    added recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -17,4 +17,28 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ("id", "name")
+        read_only_fields = ("id",)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Serializer for ingredient objects """
+
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            "id",
+            "title",
+            "price",
+            "time_minutes",
+            "link",
+            "ingredients",
+            "tags",
+        )
         read_only_fields = ("id",)
