bf0cda8ea65c183c4afba8fcac0f3c2bbbc10dd7
alex <zed-chi@mail.ru>
2019-10-09 16:24:44 +0300

    added test for img file uuid func

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
+from unittest.mock import patch
 
 
 def sample_user(email="test@tes.ru", password="secret"):
@@ -59,3 +60,11 @@ class ModelTests(TestCase):
             price=5.00,
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch("uuid.uuid4")
+    def test_recipe_imgfile_name_uuid(self, mock_uuid):
+        uuid = "test"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "myimage.jpg")
+        exp_path = f"uploads/recipe/{uuid}.jpg"
+        self.assertEqual(file_path, exp_path)
